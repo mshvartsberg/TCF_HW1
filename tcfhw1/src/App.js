a907b0f0a526b4b13060109bba6df544a94e56e6
mshvartsberg <meggan567@gmail.com>
2024-02-18 11:45:07 -0500

    created react app+wrote reasons for merge conflict

---
+++ tcfhw1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Common Reasons for Merge Conflicts:</h1>
+      <h3>1. Two people changed the same lines in a file</h3>
+      <h3>2. Someone deleted a file while another was modifying it</h3>
+      <h3>3. Two people edit the function in an incompatible way (i.e. different amounts of parameters)</h3>
+    </div>
+  );
+}
+
+export default App;
